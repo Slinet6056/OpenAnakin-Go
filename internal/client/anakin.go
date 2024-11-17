38ae39a5be8e0f58132b23673f3532a8cd74014b
Slinet6056 <inbox@slinet.me>
2024-11-17 13:42:07 +0800

    移除超时限制

--- internal/client/anakin.go
+++ internal/client/anakin.go
@@ -40,18 +40,13 @@ type AnakinClient struct {
 func NewAnakinClient(modelAppIds map[string]int) *AnakinClient {
 	return &AnakinClient{
 		httpClient: &http.Client{
-			Timeout: time.Second * 30,
 			Transport: &http.Transport{
 				DialContext: (&net.Dialer{
-					Timeout:   5 * time.Second,
 					KeepAlive: 30 * time.Second,
 				}).DialContext,
-				TLSHandshakeTimeout:   5 * time.Second,
-				ResponseHeaderTimeout: 10 * time.Second,
-				ExpectContinueTimeout: 1 * time.Second,
-				MaxIdleConns:          100,
-				MaxConnsPerHost:       100,
-				IdleConnTimeout:       90 * time.Second,
+				MaxIdleConns:     100,
+				MaxConnsPerHost:  100,
+				IdleConnTimeout:  90 * time.Second,
 			},
 		},
 		modelAppIds: modelAppIds,
